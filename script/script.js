f15898222160461f8fd173c5131485147f06b42b
Kate <braumwinter@gmail.com>
2020-12-16 15:13:32 +0300

    feat: add review slider

--- script/script.js
+++ script/script.js
@@ -109,7 +109,7 @@ main_slider_control_right.addEventListener('click', () => {
 
 main_slider.addEventListener('mouseenter', function () {
     main_slider_config.is_cycling = false;
-        clearInterval(main_slider_interval);
+    clearInterval(main_slider_interval);
 });
 
 main_slider.addEventListener('mouseleave', function () {
@@ -123,7 +123,7 @@ main_slider.addEventListener('mouseleave', function () {
 const activities_slider = document.getElementById('activities_slider');
 const activities_slider_wrapper = document.querySelector('.activities_slider_wrapper');
 const activities_slider_items = document.querySelectorAll('.activities_slider_img');
-const activities_slider_controls = document.querySelectorAll('.main_slider_control');
+const activities_slider_controls = document.querySelectorAll('.activities_slider_control');
 const activities_slider_control_left = document.getElementById('activities_slider_arrow_left');
 const activities_slider_control_right = document.getElementById('activities_slider_arrow_right');
 const activities_slider_wrapper_width = parseFloat(getComputedStyle(activities_slider_wrapper).width);
@@ -231,11 +231,156 @@ activities_slider_control_right.addEventListener('click', () => {
 
 activities_slider.addEventListener('mouseenter', function () {
     activities_slider_config.is_cycling = false;
-        clearInterval(activities_slider_interval);
+    clearInterval(activities_slider_interval);
 });
 
 activities_slider.addEventListener('mouseleave', function () {
     activities_slider_config.is_cycling = true;
     clearInterval(activities_slider_interval);
     activities_slider_cycle(activities_slider_config.direction);
+});
+
+
+/////////////////////////////////////////////////////////////////////
+
+const review_slider = document.getElementById('review_slider');
+const review_slider_wrapper = document.querySelector('.review_slider_wrapper');
+const review_slider_items = document.querySelectorAll('.review_card');
+const review_slider_controls = document.querySelectorAll('.review_slider_control');
+const review_slider_control_left = document.getElementById('review_slider_navigation_arrow_left');
+const review_slider_control_right = document.getElementById('review_slider_navigation_arrow_right');
+const review_slider_navigation_list = document.getElementById('review_slider_navigation_list');
+const review_slider_wrapper_width = parseFloat(getComputedStyle(review_slider_wrapper).width);
+const review_slider_item_width = parseFloat(getComputedStyle(review_slider_items[0]).width);
+let review_slider_position_left_item = 0;
+let review_slider_transform = 0;
+const review_slider_step = review_slider_item_width / review_slider_wrapper_width * 100;
+let review_slider_arr_items = [];
+let review_slide_active_number = 0;
+
+review_slider_items.forEach(function (item, index) {
+    item.setAttribute('review_slider_number', index);
+    if(index === 0) {
+        item.setAttribute('is_review_slider_activ', true);
+    } else {
+        item.setAttribute('is_review_slider_activ', false);
+    }
+    
+    review_slider_arr_items.push({
+        id: index,
+        item: item,
+        position: index,
+        transform: 0
+    });
+
+    const review_navigation_item = document.createElement('li');
+    review_navigation_item.className = 'review_slider_navigation_item';
+
+    review_navigation_item.setAttribute('review_slider_navigation_number', index);
+
+    if(index === 0) {
+        review_navigation_item.classList.add('review_slider_navigation_item_active');
+    }
+
+    review_slider_navigation_list.appendChild(review_navigation_item);
+});
+
+let init_review_slider_arr_items = JSON.parse(JSON.stringify(review_slider_arr_items));
+
+const review_slider_navigation_items = document.querySelectorAll('.review_slider_navigation_item');
+
+const review_slider_position = {
+    get_item_min: function () {
+        let index_item = 0;
+        review_slider_arr_items.forEach(function (item, index) {
+            if (item.position < review_slider_arr_items[index_item].position) {
+                index_item = index;
+            }
+        });
+        return index_item;
+    },
+    get_item_max: function () {
+        let index_item = 0;
+        review_slider_arr_items.forEach(function (item, index) {
+            if (item.position > review_slider_arr_items[index_item].position) {
+                index_item = index;
+            }
+        });
+        return index_item;
+    },
+    get_min: function () {
+        return review_slider_arr_items[review_slider_position.get_item_min()].position;
+    },
+    get_max: function () {
+        return review_slider_arr_items[review_slider_position.get_item_max()].position;
+    }
+}
+
+function review_slider_transform_item (direction) {
+    let next_item;
+    if (direction === 'right') {
+        review_slider_position_left_item++;
+        review_slide_active_number++;
+
+        console.log('review_slider_position_left_item', review_slider_position_left_item);
+        console.log('review_slide_active_number', review_slide_active_number);
+
+        if ((review_slider_position_left_item + review_slider_wrapper_width / review_slider_item_width - 1) > review_slider_position.get_max()) {
+
+
+            next_item = review_slider_position.get_item_min();
+            review_slider_arr_items[next_item].position = review_slider_position.get_max() + 1;
+            review_slider_arr_items[next_item].transform += review_slider_arr_items.length * 100;
+            review_slider_arr_items[next_item].item.style.transform = 'translateX(' + review_slider_arr_items[next_item].transform + '%)';
+        }
+    
+        review_slider_transform -= review_slider_step;
+    }
+    if (direction === 'left') {
+        review_slider_position_left_item--;
+        review_slide_active_number--;
+
+        if (review_slider_position_left_item < review_slider_position.get_min()) {
+            
+            
+            next_item = review_slider_position.get_item_max();
+            review_slider_arr_items[next_item].position = review_slider_position.get_min() - 1;
+            review_slider_arr_items[next_item].transform -= review_slider_arr_items.length * 100;
+            review_slider_arr_items[next_item].item.style.transform = 'translateX(' + review_slider_arr_items[next_item].transform + '%)';
+        }
+
+        review_slider_transform += review_slider_step;
+    }
+    review_slider_wrapper.style.transform = 'translateX(' + review_slider_transform + '%)';
+
+    if(review_slide_active_number > 2) {
+        review_slide_active_number -= 3;
+    }
+
+    review_slider_navigation_items.forEach(function (item) {
+        item.classList.remove('review_slider_navigation_item_active');
+
+        if(+item.getAttribute('review_slider_navigation_number') === review_slide_active_number) {
+            item.classList.add('review_slider_navigation_item_active');
+        }
+    });
+}
+
+review_slider_control_left.addEventListener('click', () => {
+    review_slider_transform_item('left');
+}, false);
+
+review_slider_control_right.addEventListener('click', () => {
+    review_slider_transform_item('right');
+}, false);
+
+review_slider_navigation_items.forEach(function (item, index) {
+    item.addEventListener('click', () => {
+        console.log(index, 'navigation');
+        review_slider_position_left_item = item.getAttribute('review_slider_navigation_number');
+        review_slide_active_number = item.getAttribute('review_slider_navigation_number');
+        review_slider_arr_items = JSON.parse(JSON.stringify(init_review_slider_arr_items));
+        review_slider_wrapper.style.transform = 'translateX(' + ( -review_slider_step * index) + '%)';
+        console.log(review_slider_arr_items);
+    }, false);
 });
